8456829f89ceab9f185811acad05ad9be967b396
ImsirovicAjdin <ajdika@live.com>
2019-10-06 19:33:25 +0200

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app/app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { AppComponent } from './app-component/app.component';
+import { NavbarComponent } from './navbar-component/navbar.component';
+import { HeadingComponent } from './heading-component/heading.component';
+import { BlogPostsComponent } from './blog-posts-component/blog-posts.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NavbarComponent ],
+  declarations: [ AppComponent, NavbarComponent, HeadingComponent, BlogPostsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/blog-posts-component/blog-posts.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-posts',
+  templateUrl: './blog-posts.component.html'
+})
+export class BlogPostsComponent  { }
