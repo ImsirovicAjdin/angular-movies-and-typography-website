92c5c3c6bab077a859bfdfb412e900f7d8ae0386
ImsirovicAjdin <ajdika@live.com>
2019-10-06 19:48:59 +0200

    Complete-the-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app-component/app.component';
 import { NavbarComponent } from './navbar-component/navbar.component';
 import { HeadingComponent } from './heading-component/heading.component';
 import { BlogPostsComponent } from './blog-posts-component/blog-posts.component';
+import { FooterComponent } from './footer-component/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NavbarComponent, HeadingComponent, BlogPostsComponent ],
+  declarations: [ AppComponent, NavbarComponent, HeadingComponent, BlogPostsComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
